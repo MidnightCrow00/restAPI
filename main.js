7a4681ac130d52457728713496ec08ae9dd271c1
MidnightCrow00 <orsolyaalbert1@gmail.com>
2024-05-10 09:28:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import { getAdat } from "./async.js";
+
+console.log("Róka Rudi");
+
+getAdat("adatok.js", megjelenit)
+getAdat( "https://pokeapi.co/api/v2/pokemon/ditto", megjelenitPokemon)
+
+console.log("Balett");
+
+function megjelenit(adat) {
+    $("body").append(`<h2>${adat.termek[0].nev}</h2>`)
+}
+function megjelenitPokemon(adat) {
+    $("body").append(`<h1>Pokemonok</h1>`)
+    $("body").append(`<h2>${adat.name}</h2>`)
+    $("body").append(`<img src="${adat.sprites.front_default}" alt="${adat.name}">`)
+}
